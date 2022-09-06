10f8b0fd957d9778a1fe375d5356c3e00a7fa84e
Felix Brockmeier <felix.brockmeier@outlook.com>
2022-09-06 20:19:31 +0200

    added account logic

--- src/config.ts
+++ src/config.ts
@@ -11,6 +11,39 @@ export interface AuthSchedulerEnvConfig {
   tokenUrl: string;
 }
 
+const getAuthSchedulerEnvConfig = (): AuthSchedulerEnvConfig => {
+  switch (mode) {
+    case 'development': {
+      const clientSecret = process.env.SYSTEM_INTERNAL_AUTH_CLIENT_SECRET_DEV || '';
+      if (!clientSecret) throw new Error('auth client secret missing');
+
+      const clientId = '3o029nji154v0bm109hkvkoi5h';
+      const tokenUrl =
+        'https://auth-cito-dev.auth.eu-central-1.amazoncognito.com/oauth2/token';
+      return { clientSecret, clientId, tokenUrl };
+    }
+    case 'test': {
+      const clientSecret =
+        process.env.AUTH_SCHEDULER_CLIENT_SECRET_STAGING || '';
+      if (!clientSecret) throw new Error('auth client secret missing');
+
+      const clientId = '';
+      const tokenUrl = '';
+      return { clientSecret, clientId, tokenUrl };
+    }
+    case 'production': {
+      const clientSecret = process.env.SYSTEM_INTERNAL_AUTH_CLIENT_SECRET_PROD || '';
+      if (!clientSecret) throw new Error('auth client secret missing');
+
+      const clientId = '54n1ig9sb07d4d9tiihdi0kifq';
+      const tokenUrl = 'https://auth.citodata.com/oauth2/token';
+      return { clientSecret, clientId, tokenUrl };
+    }
+    default:
+      throw new Error('node env misconfiguration');
+  }
+};
+
 export const appConfig = {
   express: {
     port,
@@ -22,5 +55,6 @@ export const appConfig = {
       bcc: ['felix@citodata.com', 'clemens@citodata.com'],
       source: 'felix@citodata.com'
     },
+    authSchedulerEnvConfig: getAuthSchedulerEnvConfig(),
   },
 };
--- src/lambda.ts
+++ src/lambda.ts
@@ -1,4 +1,40 @@
+import axios, { AxiosRequestConfig } from 'axios';
+import { appConfig } from './config';
+import createAccount from './create-account';
 import sendEmail from './send-email';
+
+
+const getJwt = async (): Promise<string> => {
+  try {
+    const config: AxiosRequestConfig = {
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          `${appConfig.cloud.authSchedulerEnvConfig.clientId}:${appConfig.cloud.authSchedulerEnvConfig.clientSecret}`
+        ).toString('base64')}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      params: new URLSearchParams({
+        grant_type: 'client_credentials',
+        client_id: appConfig.cloud.authSchedulerEnvConfig.clientId,
+      }),
+    };
+
+    const response = await axios.post(
+      appConfig.cloud.authSchedulerEnvConfig.tokenUrl,
+      undefined,
+      config
+    );
+    const jsonResponse = response.data;
+    if (response.status !== 200) throw new Error(jsonResponse.message);
+    if (!jsonResponse.access_token)
+      throw new Error('Did not receive an access token');
+    return jsonResponse.access_token;
+  } catch (error: unknown) {
+    if (typeof error === 'string') return Promise.reject(error);
+    if (error instanceof Error) return Promise.reject(error.message);
+    return Promise.reject(new Error('Unknown error occured'));
+  }
+};
 
 // eslint-disable-next-line import/prefer-default-export
 export const handler = async (
@@ -9,6 +45,17 @@ export const handler = async (
   callback: any
 ): Promise<void> => {
   try {
+
+    const { userName } = event;
+    const jwt = await getJwt();
+
+    const createAccountResult = await createAccount(userName, jwt);
+
+    if(!createAccountResult.success) {
+      callback(`--> ${createAccountResult.error}`, event);
+      return;
+    }
+
     // eslint-disable-next-line @typescript-eslint/naming-convention
     const { given_name } = event.request.userAttributes;
     if (!given_name) throw new Error('First name not available');
